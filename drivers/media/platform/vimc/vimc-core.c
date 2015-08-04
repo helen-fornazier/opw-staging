c8c435f1e94bba7b5d016630609ff6d0be6d0b20
Helen Koike <helen.koike@collabora.com>
2015-08-04 11:33:23 -0700

    [media] vimc: deb: Add debayer filter
    Implement the debayer filter and integrate it with the core
    
    Signed-off-by: Helen Koike <helen.koike@collabora.com>
    
    ---
    
    Series-changes: 2
    [media] vimc: deb: Add debayer filter
    	- Using MEDIA_ENT_F_ATV_DECODER in function
    	- remove v4l2_dev and dev from vimc_deb_device struct
    	- src fmt propagates from the sink
    	- coding style
    	- remove redundant else if statements
    	- check end of enum and remove BUG_ON
    	- enum frame size with min and max values
    	- set/try fmt
    	- remove unecessary include freezer.h
    	- check pad types on create
    	- return EBUSY when trying to set the format while stream is on
    	- remove vsd struct
    	- add IS_SRC and IS_SINK macros
    	- add deb_mean_win_size as a parameter of the module
    	- check set_fmt default parameters for quantization, colorspace ...
    	- add more dev_dbg

--- drivers/media/platform/vimc/vimc-core.c
+++ drivers/media/platform/vimc/vimc-core.c
@@ -23,6 +23,7 @@
 
 #include "vimc-capture.h"
 #include "vimc-core.h"
+#include "vimc-debayer.h"
 #include "vimc-sensor.h"
 
 #define VIMC_PDEV_NAME "vimc"
@@ -637,9 +638,12 @@ static int vimc_device_register(struct vimc_device *vimc)
 			create_func = vimc_cap_create;
 			break;
 
+		case VIMC_ENT_NODE_DEBAYER:
+			create_func = vimc_deb_create;
+			break;
+
 		/* TODO: Instantiate the specific topology node */
 		case VIMC_ENT_NODE_INPUT:
-		case VIMC_ENT_NODE_DEBAYER:
 		case VIMC_ENT_NODE_SCALER:
 		default:
 			/*
