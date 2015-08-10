210b1960a863e6aaccc7d2e9daaf0ea64304603f
Helen Fornazier <helen.fornazier@gmail.com>
2015-08-10 15:42:43 -0700

    tmp: cfg: adding list structures

--- drivers/media/platform/vimc/vimc-configfs.c
+++ drivers/media/platform/vimc/vimc-configfs.c
@@ -19,6 +19,68 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 
+#include "vimc-configfs.h"
+
+#define VIMC_CFG_ROLE_SENSOR_STR	"sensor"
+#define VIMC_CFG_ROLE_CAPTURE_STR	"capture"
+#define VIMC_CFG_ROLE_INPUT_STR		"input"
+#define VIMC_CFG_ROLE_DEBAYER_STR	"debayer"
+#define VIMC_CFG_ROLE_SCALER_STR	"scaler"
+
+enum vimc_cfg_status {
+	VIMC_CFG_DEPLOYED,
+	VIMC_CFG_UNDEPLOYED,
+	VIMC_CFG_ERROR,
+};
+
+/* Structure which describes a pad within a entity */
+struct vimc_cfg_pad {
+	struct config_group group;
+	struct list_head list;
+
+	int direction;
+};
+
+/* Structure which describes individual configuration for each entity */
+struct vimc_cfg_ent {
+	struct config_group group;
+	struct list_head list;
+	struct list_head pads;
+	size_t num_pads;
+	enum vimc_cfg_role role;
+	const char *name;
+};
+
+/* Structure which describes links between entities */
+struct vimc_cfg_link {
+	struct config_item item;
+	struct list_head list;
+	unsigned long flags;
+	struct vimc_cfg_pad *src_pad;
+	struct vimc_cfg_pad *sink_pad;
+};
+
+/* Structure which describes the whole topology */
+struct vimc_cfg_pipeline {
+	/* List of vimc_cfg_ent structures */
+	struct list_head ents;
+	/* List of vimc_cfg_link structures */
+	struct list_head links;
+};
+
+/* Structure which describes a platform device instance */
+struct vimc_cfg_pdev {
+	struct config_group group;
+	struct vimc_cfg_pipeline pipe;
+	struct platform_device pdev;
+};
+
+/* Struct which describes the subsystem */
+struct vimc_cfg_subsys {
+	struct configfs_subsystem subsys;
+	unsigned int pdev_id;
+};
+
 /* --------------------------------------------------------------------------
  * Link
  */
@@ -47,7 +109,13 @@ static ssize_t vimc_cfg_lnk_show_attr(struct config_item *item,
 
 static void vimc_cfg_lnk_release_item(struct config_item *item)
 {
-	kfree(item);
+	struct vimc_cfg_link *link = container_of(item,
+						  struct vimc_cfg_link, item);
+
+	/* Remove link item from the dev list */
+	list_del(&link->list);
+
+	kfree(link);
 }
 
 static struct configfs_item_operations vimc_cfg_lnk_item_ops = {
@@ -70,13 +138,29 @@ static struct config_item_type vimc_cfg_lnk_type = {
 static struct config_item *vimc_cfg_lnk_make_item(struct config_group *group,
 						  const char *name)
 {
-	struct config_item *item = kzalloc(sizeof(struct config_item), GFP_KERNEL);
-	if (!item)
+	struct vimc_cfg_link *link;
+	struct vimc_cfg_ent *ent;
+	struct vimc_cfg_pad *pad;
+	struct vimc_cfg_pdev *pd;
+
+	link = kzalloc(sizeof(struct vimc_cfg_link), GFP_KERNEL);
+	if (!link)
 		return ERR_PTR(-ENOMEM);
 
-	config_item_init_type_name(item, name, &vimc_cfg_lnk_type);
+	config_item_init_type_name(&link->item, name, &vimc_cfg_lnk_type);
 
-	return item;
+	/* Save parent pad as a source pad */
+	pad = container_of(group, struct vimc_cfg_pad, group);
+	link->src_pad = pad;
+
+	/* Add item to the dev list */
+	ent = container_of(pad->group.cg_item.ci_parent,
+			   struct vimc_cfg_ent, group.cg_item);
+	pd = container_of(ent->group.cg_item.ci_parent,
+			   struct vimc_cfg_pdev, group.cg_item);
+	list_add_tail(&link->list, &pd->pipe.links);
+
+	return &link->item;
 }
 
 /* --------------------------------------------------------------------------
@@ -107,7 +191,14 @@ static ssize_t vimc_cfg_pad_show_attr(struct config_item *item,
 
 static void vimc_cfg_pad_release_item(struct config_item *item)
 {
-	kfree(item);
+	struct vimc_cfg_pad *pad = container_of(item,
+						struct vimc_cfg_pad,
+						group.cg_item);
+
+	/* Remove this entity from the parent's list */
+	list_del(&pad->list);
+
+	kfree(pad);
 }
 
 static struct configfs_item_operations vimc_cfg_pad_item_ops = {
@@ -133,16 +224,24 @@ static struct config_item_type vimc_cfg_pad_type = {
 	.ct_owner	= THIS_MODULE,
 };
 
-static struct config_group *vimc_cfg_pad_make_group(struct config_group *pgroup,
+static struct config_group *vimc_cfg_pad_make_group(struct config_group *group,
 						    const char *name)
 {
-	struct config_group *group = kzalloc(sizeof(struct config_group), GFP_KERNEL);
-	if (!group)
+	struct vimc_cfg_pad *pad;
+	struct vimc_cfg_ent *ent;
+
+	pad = kzalloc(sizeof(struct vimc_cfg_pad), GFP_KERNEL);
+	if (!pad)
 		return ERR_PTR(-ENOMEM);
 
-	config_group_init_type_name(group, name, &vimc_cfg_pad_type);
+	config_group_init_type_name(&pad->group, name, &vimc_cfg_pad_type);
 
-	return group;
+	/* Add this entity in the parent's list */
+	ent = container_of(group, struct vimc_cfg_ent, group);
+	list_add_tail(&pad->list, &ent->pads);
+	ent->num_pads++;
+
+	return &pad->group;
 }
 
 /* --------------------------------------------------------------------------
@@ -165,16 +264,20 @@ static ssize_t vimc_cfg_ent_show_attr(struct config_item *item,
 				      struct configfs_attribute *attr,
 				      char *page)
 {
-	/* TODO: take the according action dependending on the attribute being
-	 * shown */
 	return sprintf(page, attr->ca_name);
 }
 
 static void vimc_cfg_ent_release_item(struct config_item *item)
 {
-	struct config_group *group = container_of(item, struct config_group,
-						  cg_item);
-	kfree(group);
+	struct vimc_cfg_ent *ent = container_of(item,
+						struct vimc_cfg_ent,
+						group.cg_item);
+
+	pr_info("ent_release: ent %p\n", ent);
+	/* Remove this entity from the parent's list */
+	list_del(&ent->list);
+
+	kfree(ent);
 }
 
 static struct configfs_item_operations vimc_cfg_ent_item_ops = {
@@ -200,70 +303,157 @@ static struct config_item_type vimc_cfg_ent_type = {
 	.ct_owner	= THIS_MODULE,
 };
 
-static struct config_group *vimc_cfg_ent_make_group(struct config_group *pgroup,
+static struct config_group *vimc_cfg_ent_make_group(struct config_group *group,
 						    const char *name)
 {
-	struct config_group *group = kzalloc(sizeof(struct config_group), GFP_KERNEL);
-	if (!group)
+	struct vimc_cfg_pdev *pd;
+	struct vimc_cfg_ent *ent;
+
+	ent = kzalloc(sizeof(struct vimc_cfg_ent), GFP_KERNEL);
+	if (!ent)
 		return ERR_PTR(-ENOMEM);
 
-	config_group_init_type_name(group, name, &vimc_cfg_ent_type);
+	pr_info("ent_make_group: ent %p\n", ent);
 
-	return group;
+	INIT_LIST_HEAD(&ent->pads);
+	config_group_init_type_name(&ent->group, name, &vimc_cfg_ent_type);
+
+	/* Add this entity in the parent's list */
+	pd = container_of(group, struct vimc_cfg_pdev, group);
+	pr_info("ent_make_group: pdev %p\n", pd);
+	pr_info("ent_make_group: list %p prev %p next %p\n", &pd->pipe.ents,
+		pd->pipe.ents.prev, pd->pipe.ents.next);
+
+	pr_info("ent_make_group: ent_list %p prev %p next %p\n", &ent->list,
+		ent->list.prev, ent->list.next);
+	list_add_tail(&ent->list, &pd->pipe.ents);
+
+	return &ent->group;
+}
+
+/* --------------------------------------------------------------------------
+ * Device instance
+ */
+
+static struct configfs_attribute vimc_cfg_pdev_attr_deploy = {
+	.ca_owner = THIS_MODULE,
+	.ca_name = "deploy",
+	.ca_mode = S_IRUGO,
+};
+
+static void vimc_cfg_pdev_release_item(struct config_item *item)
+{
+	struct vimc_cfg_pdev *cfg_pdev = container_of(item,
+						      struct vimc_cfg_pdev,
+						      group.cg_item);
+
+	/* TODO: check how we prohibit rmdir if the dev is in use */
+	vimc_pdev_unregister(&cfg_pdev->pdev);
+	/* Check if we can free the pdev here */
+	kfree(cfg_pdev);
+}
+
+static ssize_t vimc_cfg_pdev_store_attr(struct config_item *item,
+					struct configfs_attribute *attr,
+					const char *page, size_t count)
+{
+	/* TODO */
+	return 0;
+}
+
+static ssize_t vimc_cfg_pdev_show_attr(struct config_item *item,
+				       struct configfs_attribute *attr,
+				       char *page)
+{
+	/* TODO */
+	return sprintf(page, attr->ca_name);
+}
+
+static struct configfs_item_operations vimc_cfg_pdev_item_ops = {
+	.show_attribute		= vimc_cfg_pdev_show_attr,
+	.store_attribute	= vimc_cfg_pdev_store_attr,
+	.release		= vimc_cfg_pdev_release_item,
+};
+
+static struct configfs_attribute *vimc_cfg_pdev_attrs[] = {
+	&vimc_cfg_pdev_attr_deploy,
+	NULL,
+};
+
+static struct configfs_group_operations vimc_cfg_pdev_group_ops = {
+	/* Create pad children */
+	.make_group	= vimc_cfg_ent_make_group,
+};
+
+static struct config_item_type vimc_cfg_pdev_type = {
+	.ct_item_ops	= &vimc_cfg_pdev_item_ops,
+	.ct_group_ops	= &vimc_cfg_pdev_group_ops,
+	.ct_attrs	= vimc_cfg_pdev_attrs,
+	.ct_owner	= THIS_MODULE,
+};
+
+static void vimc_cfg_pdev_release(struct device *dev)
+{}
+
+static struct config_group *vimc_cfg_pdev_make_group(struct config_group *group,
+						    const char *name)
+{
+	struct vimc_cfg_subsys *cfg_subsys;
+	struct vimc_cfg_pdev *pd;
+	int ret;
+
+	pd = kzalloc(sizeof(struct vimc_cfg_pdev), GFP_KERNEL);
+	if (!pd)
+		return ERR_PTR(-ENOMEM);
+
+	pr_info("pdev_make_group: pdev %p\n", pd);
+
+	/* Get the parent subsystem */
+	cfg_subsys = container_of(group->cg_subsys,
+				  struct vimc_cfg_subsys, subsys);
+
+	pd->pdev.name = group->cg_item.ci_name;
+	pd->pdev.dev.release = vimc_cfg_pdev_release;
+	/* TODO: release the ids? Or can we assume unsigned
+	 * int is big enough? */
+	pd->pdev.id = cfg_subsys->pdev_id++;
+	ret = vimc_pdev_register(&pd->pdev);
+	if (ret) {
+		kfree(pd);
+		return ERR_PTR(ret);
+	}
+
+	INIT_LIST_HEAD(&pd->pipe.ents);
+	INIT_LIST_HEAD(&pd->pipe.links);
+
+	pr_info("pdev_make_group: list %p prev %p next %p\n", &pd->pipe.ents,
+		pd->pipe.ents.prev, pd->pipe.ents.next);
+
+	config_group_init_type_name(&pd->group, name, &vimc_cfg_pdev_type);
+
+	return &pd->group;
 }
 
 /* --------------------------------------------------------------------------
  * Subsystem
  */
 
-static struct configfs_attribute vimc_cfg_subsys_attr_deploy = {
-	.ca_owner = THIS_MODULE,
-	.ca_name = "deploy_topology",
-	.ca_mode = S_IRUGO,
-};
-
-static struct configfs_attribute vimc_cfg_subsys_attr_status = {
-	.ca_owner = THIS_MODULE,
-	.ca_name = "status",
-	.ca_mode = S_IRUGO,
-};
-
-static ssize_t vimc_cfg_subsys_show_attr(struct config_item *item,
-					 struct configfs_attribute *attr,
-					 char *page)
-{
-	/* TODO: take the according action dependending on the attribute being
-	 * shown */
-	return sprintf(page, attr->ca_name);
-}
-
-static struct configfs_item_operations vimc_cfg_subsys_item_ops = {
-	.show_attribute	= vimc_cfg_subsys_show_attr,
-};
-
 static struct configfs_group_operations vimc_cfg_subsys_group_ops = {
 	/* Create entity children */
-	.make_group	= vimc_cfg_ent_make_group,
-};
-
-static struct configfs_attribute *vimc_cfg_subsys_attrs[] = {
-	&vimc_cfg_subsys_attr_deploy,
-	&vimc_cfg_subsys_attr_status,
-	NULL,
+	.make_group	= vimc_cfg_pdev_make_group,
 };
 
 static struct config_item_type vimc_cfg_subsys_type = {
-	.ct_item_ops	= &vimc_cfg_subsys_item_ops,
 	.ct_group_ops	= &vimc_cfg_subsys_group_ops,
-	.ct_attrs	= vimc_cfg_subsys_attrs,
 	.ct_owner	= THIS_MODULE,
 };
 
-static struct configfs_subsystem vimc_cfg_subsys = {
-	.su_group = {
-		.cg_item = {
-			.ci_namebuf = "vimc",
-			.ci_type = &vimc_cfg_subsys_type,
+static struct vimc_cfg_subsys vimc_cfg_subsys = {
+	.subsys = {
+		.su_group = {
+			.cg_item = {
+				.ci_type = &vimc_cfg_subsys_type,
+			},
 		},
 	},
 };
@@ -272,25 +462,23 @@ static struct configfs_subsystem vimc_cfg_subsys = {
  * Functions
  */
 
-int vimc_cfg_register(void)
+int vimc_cfg_register(char *name)
 {
 	struct configfs_subsystem *subsys;
-	int ret = 0;
+	int ret;
 
-	subsys = &vimc_cfg_subsys;
+	BUG_ON(!name);
+
+	subsys = &vimc_cfg_subsys.subsys;
+	subsys->su_group.cg_item.ci_name = name;
 	config_group_init(&subsys->su_group);
 	mutex_init(&subsys->su_mutex);
 	ret = configfs_register_subsystem(subsys);
-	if (ret) {
-		printk(KERN_ERR "Error %d while registering subsystem %s\n",
-		       ret,
-		       subsys->su_group.cg_item.ci_namebuf);
-	}
 
 	return ret;
 }
 
 void vimc_cfg_unregister(void)
 {
-	configfs_unregister_subsystem(&vimc_cfg_subsys);
+	configfs_unregister_subsystem(&vimc_cfg_subsys.subsys);
 }
--- drivers/media/platform/vimc/vimc-configfs.h
+++ drivers/media/platform/vimc/vimc-configfs.h
@@ -20,7 +20,34 @@
 
 #include "vimc-core.h"
 
-int vimc_cfg_register(void);
+/**
+ * enum vimc_cfg_role - Select the functionality of a node in the topology
+ * @VIMC_CFG_ROLE_SENSOR:	A node with role SENSOR simulates a camera sensor
+ *				generating internal images in bayer format and
+ *				propagating those images through the pipeline
+ * @VIMC_CFG_ROLE_CAPTURE:	A node with role CAPTURE is a v4l2 video_device
+ *				that exposes the received image from the
+ *				pipeline to the user space
+ * @VIMC_CFG_ROLE_INPUT:	A node with role INPUT is a v4l2 video_device that
+ *				receives images from the user space and
+ *				propagates them through the pipeline
+ * @VIMC_CFG_ROLE_DEBAYER:	A node with role DEBAYER expects to receive a frame
+ *				in bayer format converts it to RGB
+ * @VIMC_CFG_ROLE_SCALER:	A node with role SCALER scales the received image
+ *				by a given multiplier
+ *
+ * This enum is used in the entity configuration struct to allow the definition
+ * of a custom topology specifying the role of each role on it.
+ */
+enum vimc_cfg_role {
+	VIMC_CFG_ROLE_SENSOR,
+	VIMC_CFG_ROLE_CAPTURE,
+	VIMC_CFG_ROLE_INPUT,
+	VIMC_CFG_ROLE_DEBAYER,
+	VIMC_CFG_ROLE_SCALER,
+};
+
+int vimc_cfg_register(char *drv_name);
 
 void vimc_cfg_unregister(void);
 
